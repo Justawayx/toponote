d8a3b6341e197377152ae32a94a6d610bdeb682f
Annie Gao <annie.gao@yale.edu>
2021-02-14 00:43:30 -0500

    pages seems to be working

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testAPIRouter = require('./routes/testAPI');
 var pagesRouter = require('./routes/pages');
+var notesRouter = require('./routes/notes');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/testAPI', testAPIRouter);
 app.use('/pages', pagesRouter);
+app.use('/notes', notesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- frontend/src/Pages.js
+++ frontend/src/Pages.js
@@ -1,46 +1,73 @@
 import React, {Component} from "react";
 import Note from './components/Note';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 class Pages extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        curr_page: "",
+        curr_page_name: "",
+        curr_page_id: -1,
         notes: [],
         pages: []
       };
-      this.getPages = this.getPages.bind(this);
+      this.newPageName = React.createRef();
       this.handleClick = this.handleClick.bind(this);
+      this.getPages = this.getPages.bind(this);
+      this.deletePage = this.deletePage.bind(this);
     }
 
-    componentWillMount() {
-        this.setState({ curr_page: "", notes: [] });
+    componentDidMount() {
         this.getPages();
     }
 
     getPages() {
         fetch("http://localhost:9000/pages")
-            .then(response => response.json())
+            .then(response => {
+                console.log("response: " + JSON.stringify(response));
+                return response.json();
+            })
             .then(data => {
-                this.setState({ pages: data })
+                console.log("data: " + JSON.stringify(data));
+                this.setState({ pages: data });
             });
     }
 
-    handleClick(pageName) {
-        fetch("http://localhost:9000/pages/getnotes")
+    handleClick(page) {
+        let pageId = page.id;
+        let pageName = page.name;
+        fetch("http://localhost:9000/notes?q=" + pageId)
             .then(response => response.json())
             .then(data => {
-                this.setState({ notes: data, curr_page: pageName });
+                console.log("BRUH " + JSON.stringify(data));
+                this.setState({ notes: data, curr_page_id: pageId, curr_page_name: pageName });
             });
+    }
+
+    createPage(pageName) {
+        fetch("http://localhost:9000/pages?q=" + pageName, { method: 'POST' })
+        .then(() => {
+            this.forceUpdate();
+        });
+    }
+
+    deletePage(pageId) {
+        fetch("http://localhost:9000/pages/" + pageId, { method: 'DELETE' })
+        .then(() => {
+            this.forceUpdate();
+        });
     }
 
     render() {
-      if (this.state.curr_page.length > 0) {
+      console.log("state: " + JSON.stringify(this.state));
+      if (this.state.curr_page_name !== -1 && this.state.curr_page_name !== "") {
         // render notes
         return (
             <div>
-                <h1>Notes for {this.state.curr_page}</h1>
+                <h1>Notes for {this.state.curr_page_name}</h1>
+                {/* if no notes, say "You have no notes in this page yet" */}
                 {this.state.notes.map(n => {
                     return <Note title={n.title} body={n.body} tags={n.tags} prereqs={n.prereqs}/>
                 })}
@@ -50,11 +77,31 @@ class Pages extends Component {
         return (
             <div>
                 <h1>My Pages</h1>
-                <ul>
+
+                <Table striped bordered hover>
+                <thead>
+                    <tr>
+                    <th>Page Name</th>
+                    <th></th>
+                    </tr>
+                </thead>
+                <tbody>
                     {this.state.pages.map(p => {
-                        return <li onClick={this.handleClick} data-id={p} key={p}>{p}</li>
+                        return (
+                            <tr>
+                            <td><a onClick={()=>this.handleClick(p)}>{p.name}</a></td>
+                            <td><Button onClick={()=>this.deletePage(p.id)}>Delete</Button></td>
+                            </tr>
+                        )
                     })}
-                </ul>
+                </tbody>
+                </Table>
+                <form onSubmit={this.createPage}>
+                    <label>New Page Title: 
+                        <input type="text" ref={this.newPageName}/>
+                    </label>
+                    <input type="submit" name="Submit"/>
+                </form>
             </div>
         );
       }
